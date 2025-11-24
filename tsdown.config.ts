05c1d3438a1cc1cf225955b9424c4af0ab6d2ac6
Madao <gn00678465@gmail.com>
2025-11-24 09:09:05 +0800

    initial commit

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  exports: true,
+  entry: [
+    './src/index.ts'
+  ],
+  format: ['esm'],
+  dts: {
+    sourcemap: true,
+  },
+})
