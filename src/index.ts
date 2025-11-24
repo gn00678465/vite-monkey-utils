d46dc403d07853b83c8f3ca51450a02766a4ab31
Madao <gn00678465@gmail.com>
2025-11-24 09:33:23 +0800

    feat: 新增 waitForElement 函數及相關選項
    新增一個 waitForElement 函數，允許根據選擇器等待 DOM 元素的出現，並提供多種選項以自定義行為。

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { waitForElement } from './utils/waitForElement.js';
---
+++ tests/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { waitForElement } from '../src'
+
+test('waitForElement', () => {
+  expect(waitForElement).toBeDefined()
+})
